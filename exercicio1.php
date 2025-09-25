9d030273203e8006d26a1552258fe41a95aac3c9
isabellialves12 <isabellialves322@gmail.com>
2025-09-25 14:17:46 -0300

    25/09

---
+++ exercicio1.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Exercicio de PHP</h1>
+
+    <form action="exercicio.php">
+
+    <p>
+        digite o nome do cliente<br>
+        <input type="text" name="cliente">
+</p>
+
+        escolha o sexo do cliente<br>
+        <input type="radio" name="sexo" value="feminino"> feminino <br>
+        <input type="radio" name="sexo" value="masculino"> masculino 
+</p>
+
+<p>
+    digite o valor da compra<br>
+    <input type="number" name="valorcompra">
+
+</p>
+
+<p>
+<input type="submit" value="envia">
+</p>
+
+</body>
+</html> 
