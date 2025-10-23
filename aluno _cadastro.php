05fb23d14d4cb71649a4da64fa3ed028d2f67f0b
isabellialves12 <isabellialves322@gmail.com>
2025-10-23 14:20:28 -0300

    23/10

--- aluno _cadastro.php
+++ aluno _cadastro.php
@@ -2,33 +2,25 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-     <h1>Cadastro de Alunos</h1>
+    <meta name="viewport" content=> ?>
 
-    <form method="POST" action="aluno_cadastro.php">
-        <label>Informe o nome do aluno:</label><br>
-        <input type="text" name="nome_aluno" required><br><br>
+    <form action="#" method="post">
+    <p>
+    digite o nome do aluno<br>
+    <input type="text" name="nome">
+    </p>
+    
+    <p>
+    digite o cidade do aluno<br>
+    <input type="text" name="cidade">
+    </p>
 
-        <label>Escolha o curso:</label><br>
-        <select name="curso" required>
-
-            <option value="Informática">Informática</option>
-            <option value="Administração">Administração</option>
-            <option value="Engenharia">Engenharia</option>
-            <option value="Direito">Direito</option>
-        </select><br><br>
-
-        <button type="submit">Cadastrar Aluno</button>
-    </form>
-
-    <?php
-
-
-
-
+    <p>
+    digite o estado do aluno<br>
+    <input type="text" name="estado">
+    </p>
+    
+  
     ?>
 </body>
 </html>
